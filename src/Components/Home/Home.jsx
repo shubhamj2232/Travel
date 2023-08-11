0e81b317ae7a9a066ab0924ad9b9ecf9c1cc8ad4
Shubham Jadhav <shubhamj0828@gmail.com>
2023-08-11 02:06:13 -0400

    Final

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './home.css'
+import video from '../../Assets/video.mp4'
+import {GrLocation} from 'react-icons/gr'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='home'>
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smalltext">
+            Our Packages
+          </span>
+          <h1 className="hometitle">
+            Search Your Holiday
+          </h1>
+        </div>
+        
+        <div className="carDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search Your Destination</label>
+            <div className="input flex">
+              <input type="text" placeholder='Enter name here...' />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
